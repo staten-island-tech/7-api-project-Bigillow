a20e84b016510cb36451f5ed340a3034ae2ee671
Bigillow <tsunheil@nycstudents.net>
2026-03-18 09:26:14 -0400

    Update app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+def getMC(mc):
+    response = requests.get(f"https://api.mcsrvstat.us/3/{mc.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    print(f"Status: {data["online"]}")
+    print(f"PLayer count: {data["players"]["online"]}")
+    print(f"Ip: {data["ip"]}")
+    print(f"Port: {data["port"]}")
+
+getMC("donutsmp.net")
