d35c9d850e15fcdc869535bff82b0e84c36c9891
Bigillow <tsunheil@nycstudents.net>
2026-03-27 08:43:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 def streak(trees):
-    wasd = int(trees)
-    streak_inc = 0
-    streak_dec = 0 
+    wasd = trees
+    streak_inc = 1
+    streak_dec = 2 
     lowest = max(wasd)
     highest = 0 
-    high = []
-    low = []
-    for tree in trees:
-        if tree > highest:
+    high = [1]
+    low = [1]
+    for tree in wasd:
+        if int(tree) > int(highest) and int(tree) != int(lowest):
             highest = tree
             streak_inc += 1
-        elif tree < highest:
-            high.append(streak_inc)
-            streak_inc = 0
-            streak_dec += 1
-        if tree > lowest:
+        elif int(tree) < int(highest) and int(tree) < int(lowest):
             lowest = tree
+            highest = 0
+            high.append(streak_inc)
+            streak_inc = 1
             streak_dec += 1
         elif tree < lowest:
+        elif int(tree) > int(highest) and int(tree) > int(lowest):
+            highest = tree
+            lowest = 0
             low.append(streak_dec)
-            streak_dec = 0
+            streak_dec = 2
             streak_inc += 1
+
+    print(max(high))
+    print(max(low))
         
-streak("1 3 4 2")
+streak("1342")
