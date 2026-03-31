ccd11c8f418373ba5a73979206743aeab53ac5ac
Bigillow <tsunheil@nycstudents.net>
2026-03-31 08:44:56 -0400

    Create sushi.py

---
+++ sushi.py
@@ -0,0 +1,24 @@
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+
+asd = {"name": "California Roll", "price": 8}
+def order(food_item):
+    receipt = {}
+    for item in food_item:
+        print(item)
+    
+
+order(asd)
+
+
+
